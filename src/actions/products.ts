b6568172fb3cf701c6c3be74347b2d364cc387c2
imakhlaq <imakhlaqXD@gmail.com>
2024-12-16 17:16:12 +0530

    example and docker-compose.yml

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -1,8 +1,20 @@
 "use server";
 
+import { auth } from "@/auth";
 import { productDetailSchema } from "@/zod_schemas/product";
+import { error } from "console";
 import { z } from "zod";
 
 export async function createProduct(
   unsafeData: z.infer<typeof productDetailSchema>
-) {}
+) {
+  const session = await auth();
+  const { success, data } = productDetailSchema.safeParse(unsafeData);
+
+  if (session?.user === null || !success) {
+    return {
+      error: true,
+      message: "There was an error while creating your product",
+    };
+  }
+}
