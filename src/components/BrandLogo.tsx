37df747a089bbc88dff1194d43b1698b66206dac
imakhlaq <imakhlaqXD@gmail.com>
2024-12-28 03:43:22 +0530

    added middleware route protection

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,5 +1,7 @@
 import { Globe2Icon } from "lucide-react";
 function BrandLogo() {
+
+    console.log("Test")
   return (
     <span className="flex items-center gap-2 font-semibold flex-shrink-0 text-lg">
       <Globe2Icon className="size-8" />
